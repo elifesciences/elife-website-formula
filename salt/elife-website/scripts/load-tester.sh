9b7f9e9264bec91f25215b51407ef80ffde87889
Chris Wilkinson <c.wilkinson@elifesciences.org>
2017-01-12 10:39:44 +0000

    Correct repo name

--- salt/elife-website/scripts/load-tester.sh
+++ salt/elife-website/scripts/load-tester.sh
@@ -6,17 +6,16 @@
 # @author: Luke Skibinski <l.skibinski@elifesciences.org>
 
 install_article_json() {
-    # clone the elife-article-json repo, update if it already exists
-    if [ ! -d elife-article-json ]; then
-        git clone https://github.com/elifesciences/elife-article-json
+    # clone the eif-article-json repo, update if it already exists
+    if [ ! -d eif-article-json ]; then
+        git clone https://github.com/elifesciences/eif-article-json
     else
-	    git -C elife-article-json reset --hard
+	    git -C eif-article-json reset --hard
     fi
 }
 
 basic_import() {
-    #for file in elife-article-json/article-json/*.json; do
-    for file in `ls elife-article-json/article-json/*.json | sort --numeric-sort --reverse`; do
+    for file in `ls eif-article-json/article-json/*.json | sort --numeric-sort --reverse`; do
         echo "POST'ing $file ..."
         time curl -v -X POST -d @$file http://localhost/api/article.json --header "Authorization: Basic {{ salt['elife.b64encode'](pillar.elife_website.drupal_users.builder.username + ':' + pillar.elife_website.drupal_users.builder.password) }}" --header "Content-Type:application/json" 2>&1 | grep 'HTTP/1.1 '
         printf "\n\n"
